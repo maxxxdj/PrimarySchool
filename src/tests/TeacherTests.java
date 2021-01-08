da099a86cddabf26c3408fbfc72b577d2a1c2838
Mrtin Rockinstone <59373264+maxxxdj@users.noreply.github.com>
2021-01-08 03:45:00 +0200

    Update TeacherTests.java

--- src/tests/TeacherTests.java
+++ src/tests/TeacherTests.java
@@ -1,44 +1,60 @@
 package tests;
 
-import Interfaces.Employee;
+import Staff.OtherStaff.Cleaner;
 import Staff.Teachers.EnglishTeacher;
 import Staff.Teachers.MathTeacher;
 import enums.Gender;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class TeacherTests {
+    EnglishTeacher englishTeacher;
+    MathTeacher mathTeacher;
+    Cleaner cleaner;
 
 
+    @Before
+    public void setUp() {
+        this.englishTeacher = new EnglishTeacher("Pamela Anderson",
+                58, 5000, Gender.FEMALE, "USA");
+
+        this.mathTeacher = new MathTeacher("Kiril Ivanov",
+                39, 860, Gender.MALE, "Bulgaria");
+        this.cleaner = new Cleaner("Maria Ivanova",
+                29, 450, Gender.FEMALE, "Bulgaria");
+
+    }
+
+    //Enlgish Teacher
     @Test       //testing lessons
     public void testEnglishTeacherLessons() {
-
-        //Enlgish Teacher
-        try {
-            EnglishTeacher teacher = new EnglishTeacher("Pamela Anderson",
-                    58, 5000, Gender.FEMALE, "USA");
-            teacher.giveLesson(20); // actual 57
-            int lessonsLeft = teacher.getLessonsAmount();
-            assertEquals(37, lessonsLeft); // expected 37
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }
+        englishTeacher.giveLesson(30); //57-30
+        int lessonsLeft = mathTeacher.getLessonsAmount();
+        assertEquals(27, lessonsLeft);            // expected 27
     }
+    // more tests to come...
 
     //Math Teacher
     @Test
     public void testMathTeacherLessons() {
-        try {
-            MathTeacher teacher = new MathTeacher("Kiril Ivanov",
-                    39, 860, Gender.MALE, "Bulgaria");
-            teacher.giveLesson(20); // actual 27
-            int lessonsLeft = teacher.getLessonsAmount();
-            assertEquals(7, lessonsLeft); // expected 7
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }
+
+        mathTeacher.giveLesson(10);  //27-10
+        int lessonsLeft = mathTeacher.getLessonsAmount(); // expected 17
+        assertEquals(17, lessonsLeft);
     }
+    // more tests to come...
+    
+    
+    //Cleaner
+    @Test
+    public void testCleanerGetSalary(){
+        cleaner.setSalary(200); //set to 200
+        assertEquals(200,cleaner.getSalary(), 0);
+    }
+    // more tests to come...
 }
 
 
