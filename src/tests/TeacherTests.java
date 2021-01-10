da36f3eb60c6730edb68c9e9e54f115b90190777
Mrtin Rockinstone <59373264+maxxxdj@users.noreply.github.com>
2021-01-11 01:34:16 +0200

    Update TeacherTests.java

--- src/tests/TeacherTests.java
+++ src/tests/TeacherTests.java
@@ -39,9 +39,10 @@ public class TeacherTests {
 
     }
 
-    //Comparator by age
+    //Comparing by age
+    //If age is equal - then sorting lexicographically
     @Test
-    public void testTeacherComparator(){
+    public void testTeacherComparatorByAge(){
         List<Teacher> sorted = teachers.stream()
                 .sorted(new TeacherComparator())
                 .collect(Collectors.toList());
