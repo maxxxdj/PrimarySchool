20cdd6281601b9867b27b5303a253c291c534ca1
Mrtin Rockinstone <59373264+maxxxdj@users.noreply.github.com>
2021-03-26 13:29:34 +0200

    Delete src directory

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.controller;
-
-import com.example.demo.entities.Employee;
-
-import java.util.List;
-
-public interface EmployeeController {
-    List<Employee> getAllEmployees();
-}
